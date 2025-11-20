6a963d1be7cbe193328d6e54264dfa9c0b8480f9
thieryus007-cloud <thieryus007@icloud.com>
2025-11-20 15:49:06 +0100

    Update screen_can_status.h

--- components/gui_lvgl/screen_can_status.h
+++ components/gui_lvgl/screen_can_status.h
@@ -1,39 +1,26 @@
 #pragma once
 
+#include "lvgl.h"
+#include "can_victron.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "lvgl.h"
-#include "event_types.h"
-
-// API C
 void screen_can_status_create(lv_obj_t *parent);
-void screen_can_status_update(const cvl_limits_event_t *limits);
+void screen_can_status_update(const can_victron_status_t *status);
 
 #ifdef __cplusplus
 }
 
 namespace gui {
-    class ScreenCanStatus {
-    public:
-        explicit ScreenCanStatus(lv_obj_t* parent);
-        void update(const cvl_limits_event_t& limits);
 
-    private:
-        lv_obj_t* root;
-        
-        // Affichage principal "Gros Chiffres"
-        lv_obj_t* label_cvl;
-        lv_obj_t* label_ccl;
-        lv_obj_t* label_dcl;
-        
-        // Status flags
-        lv_obj_t* led_imbalance;
-        lv_obj_t* led_cell_prot;
-        lv_obj_t* label_state_txt;
+class ScreenCanStatus {
+public:
+    explicit ScreenCanStatus(lv_obj_t *parent) { screen_can_status_create(parent); }
 
-        void build_ui();
-    };
-}
+    void update(const can_victron_status_t &status) { screen_can_status_update(&status); }
+};
+
+}  // namespace gui
 #endif
